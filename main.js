1531ab1a3f21f6281b0ddf37ba60ed6ef9de3dd8
juanluna96 <juandluna17@gmail.com>
2021-02-01 22:41:51 -0500

    Botón desactivar

--- main.js
+++ main.js
@@ -5,12 +5,21 @@ const app = Vue.createApp({
             cantidad: 1500,
             enlaceYoutube: 'https://youtube.com/',
             estado: true,
-            servicios: ['transferencia', 'pagos', 'giros', 'cheques']
+            servicios: ['transferencia', 'pagos', 'giros', 'cheques'],
+            desactivar: false
         }
     },
     methods: {
         AumentarCantidad() {
             this.cantidad = this.cantidad + 100;
+        },
+        DisminuirCantidad() {
+            if (this.cantidad == 0) {
+                this.desactivar = true;
+                alert('Saldo en 0');
+                return
+            }
+            this.cantidad = this.cantidad - 100;
         }
     }
 });
