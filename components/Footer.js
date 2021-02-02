9831bdc705786cff9f5538749c7c59fc2d1e33be
juanluna96 <juandluna17@gmail.com>
2021-02-02 13:46:13 -0500

    Props - Comunicación entre componentes

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,14 @@
 app.component('footer-banco', {
+    props: ['cantidad', 'fecha'],
     template: /*html*/`
     <div class="bg-dark py-3 mt-2 text-white">
         <h3 class="text-white">{{ texto }} - {{ cantidad }}</h3>
+        <p>{{ fecha }}</p>
     </div>
     `,
     data() {
         return {
             texto: 'Footer de mi sitio web',
-            cantidad: 1000
         }
     }
 })
