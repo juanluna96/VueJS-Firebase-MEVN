30f152d3ca010a11286fa9162e2bc8d49e73b62e
juanluna96 <juandluna17@gmail.com>
2021-02-01 21:44:25 -0500

    Renderizar datos de forma declarativa

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            titulo: 'Hola mundo con Vue.js v3'
+            titulo: 'Hola mundo con Vue.js v3',
+            cantidad: 1200
         }
     }
 });
