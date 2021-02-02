c8b3ac16fc3e030b7a1a9f538ccfb99d84de71ab
juanluna96 <juandluna17@gmail.com>
2021-02-01 22:13:38 -0500

    v-if v-else

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             titulo: 'Hola mundo con Vue.js v3',
-            cantidad: 1200,
+            cantidad: 1500,
             enlaceYoutube: 'https://youtube.com/',
+            estado: true
         }
     }
 });
