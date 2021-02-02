394998af1b09072044fcb7b5187059528d1373b3
juanluna96 <juandluna17@gmail.com>
2021-02-02 13:31:39 -0500

    Data en componentes

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,13 @@
 app.component('footer-banco', {
     template: /*html*/`
     <div class="bg-dark py-3 mt-2 text-white">
-        <h3 class="text-white">Footer de mi sitio web</h3>
+        <h3 class="text-white">{{ texto }} - {{ cantidad }}</h3>
     </div>
-    `
+    `,
+    data() {
+        return {
+            texto: 'Footer de mi sitio web',
+            cantidad: 1000
+        }
+    }
 })
